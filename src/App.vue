d2f8ec35d42a92c834e0b46a2dfd25a4d95c73a4
Evan Ang <alethiologist@gmail.com>
2020-09-05 01:58:41 +0800

    counter with loading bar created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app" class="container mx-auto my-8">
+        <div class="text-3xl font-bold">Leave a note for yourself!</div>
+        <Counter :notes="notes.length" />
+    </div>
 </template>
 
 <script>
+import Counter from './components/Counter.vue';
 
 export default {
-}
+    data: function() {
+        return {
+            notes: []
+        }
+    },
+    components: {
+        Counter,
+    }
+};
 </script>
 
 <style>
+/* Adds the background style */
+html {
+    background: url(assets/background.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="bar-background" class="relative w-100 bg-gray-500 h-8 rounded-md">
+        <div id="bar-text" class="absolute text-center text-white font-bold">{{ notes }} / 10</div>
+        <div id="bar-progress" class="bg-green-600 h-8 rounded-md" :style="{ width: (notes * 10) + '%' }"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["notes"],
+};
+</script>
+
+<style scoped>
+/* Centers Text */
+#bar-text {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
